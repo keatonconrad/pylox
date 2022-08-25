5ea13955d991f9afd328243fb5a7b64b154b8037
Keaton Conrad <keaton.conrad@gmail.com>
2022-08-25 17:52:09 -0500

    Simplified visitor

--- lox/visitor.py
+++ lox/visitor.py
@@ -1,24 +1,5 @@
-from abc import ABC, abstractmethod
-from expr import Binary, Grouping, Literal, Unary
-from stmt import Expression
+from abc import ABC
+
 
 class Visitor(ABC):
-    @abstractmethod
-    def visit_binary_expr(self, expr: Binary):
-        pass
-
-    @abstractmethod
-    def visit_grouping_expr(self, expr: Grouping):
-        pass
-
-    @abstractmethod
-    def visit_literal_expr(self, expr: Literal):
-        pass
-    
-    @abstractmethod
-    def visit_unary_expr(self, expr: Unary):
-        pass
-
-    @abstractmethod
-    def visit_expression_stmt(self, stmt: Expression):
-        pass
+    pass
